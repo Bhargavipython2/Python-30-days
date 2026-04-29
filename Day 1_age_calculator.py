0cce89b27d57153046558e554ea7d2b282f44a52
Bhargavipython2 <bhargavibalusupati5@gmail.com>
2026-04-30 01:41:57 +0530

    Create Day 1_age_calculator.py

---
+++ Day 1_age_calculator.py
@@ -0,0 +1,15 @@
+# Day 1 - Kalyan's First Python Program
+# Learning Python on Mobile | 6 Years Gap After Inter
+
+print("hi")
+print("nenu coding nerchukuntunanu")
+
+peru = "kalyan"
+age = 25
+print("na peru:", peru)
+print("na age:", age)
+print("vache year na age:", age + 1)
+
+user_age = int(input("Nee age entha cheppu: "))
+next_year = user_age + 1
+print("Vache year ki nee age:", next_year)
