b10de472b8b14e73c6b9c250b2b9473c541d38dd
Bhargavipython2 <bhargavibalusupati5@gmail.com>
2026-05-03 23:33:59 +0530

    Add password checker using a while loop

---
+++ Day4-password.py
@@ -0,0 +1,10 @@
+# Day 4 - While Loop by Kalyan
+print("--- Kalyan Password Checker ---")
+
+password = "" 
+while password != "kalyan123":
+    password = input("Password enter chey: ")
+    if password != "kalyan123":
+        print("Wrong password ra. Malli try chey 😅")
+
+print("Correct password! Welcome Kalyan 🔥")
