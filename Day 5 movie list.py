1b5ec295e05b5f6a5f90a217d7fdf54d08a0ff95
Bhargavipython2 <bhargavibalusupati5@gmail.com>
2026-05-04 22:07:00 +0530

    Add movie list display script

---
+++ Day 5 movie list.py
@@ -0,0 +1,10 @@
+# Day 5 - List by Kalyan
+print("--- Kalyan Movie List ---")
+
+movies = ["RRR", "Salaar", "Kalki", "Pushpa", "Bahubali"]
+
+print("Naa favorite movies:")
+for movie in movies:
+    print(movie)
+
+print("Total movies:", len(movies))
