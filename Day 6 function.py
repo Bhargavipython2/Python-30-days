b256108edaefdc7cf079df5e8e89f0b6daa30b91
Bhargavipython2 <bhargavibalusupati5@gmail.com>
2026-05-05 21:38:34 +0530

    Create greeting and addition functions in Day 6
    Added functions for greeting and adding numbers.

---
+++ Day 6 function.py
@@ -0,0 +1,16 @@
+# Day 6 - Function by Kalyan
+print("--- Kalyan Function Test ---")
+
+def kalyan_greet():
+    print("Hello Kalyan! Welcome to Day 6 🔥")
+
+def add_numbers(a, b):
+    result = a + b
+    print("Answer:", result)
+
+# Function ni pilavadam
+kalyan_greet()
+kalyan_greet()
+
+add_numbers(5, 10)
+add_numbers(20, 7)
